9c7954e61fce4a405f2f29e5b50d97cd766d3c5a
Mohammad Aldwairi <194878384+AbuMaria24@users.noreply.github.com>
2025-12-09 13:49:46 -0500

    Delete siege-control-latest directory

--- siege-control-latest/siege-control-sounds-added-keem/coin_label.gd
+++
@@ -1,6 +0,0 @@
-extends Label
-
-@onready var game_manager: Node = %GameManager
-
-func _process(delta):
-	text = "Coins: " + str(game_manager.score)
--- siege-control-latest/siege-control-sounds-added-keem/scenes/death_screen.gd
+++
@@ -1,8 +0,0 @@
-extends Node
-
-
-func _on_play_again_pressed() -> void:
-	get_tree().change_scene_to_file("res://game.tscn")
-	
-func _on_quit_pressed() -> void:
-	get_tree().quit()
